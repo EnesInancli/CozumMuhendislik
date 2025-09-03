eb5c0e242f15f896569548d0377775cf92f7f7ad
EnesInancli <eneshab1@hotmail.com>
2025-09-03 21:15:11 +0300

    Resim pathlerini public/images/ klasörüne göre güncelledim

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,11 +1,10 @@
-import { Link, useLocation } from 'react-router-dom';
+import { Link} from 'react-router-dom';
 import React, { useState } from 'react';
 import { Menu, X, ChevronDown } from 'lucide-react';
 
 export const Header: React.FC = () => {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
   const [activeDropdown, setActiveDropdown] = useState<string | null>(null);
-  const location = useLocation();
 
   const navigateToHome = () => {
     setIsMenuOpen(false);
